20a0b0428e10d5e0f8ab3ce28a6d67b24acc52bc
Oceanic <96814993+Wisof-Young@users.noreply.github.com>
2024-10-15 02:54:47 +0800

    修改一些错误

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: '🚵‍♂️Les entraînements de SAM0TA',
+  siteTitle: '🚵‍♂️SAM0TA WORKOUTS',
   siteUrl: 'https://run.oceanum.top/',
   logo: 'https://avatars.githubusercontent.com/u/96814993?s=400&u=7da8db7a41d066feca9dc0de575ff84a945651a4&v=4',
   description: '',
